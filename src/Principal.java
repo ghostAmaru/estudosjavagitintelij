e5355f867aeb50ca33ecf26a4a5ca8bd26682239
Rodrigo Conceicao <rodrigoodcrod1@gmail.com>
2020-07-23 02:28:36 -0300

    criado processo para aplicar taxa de dez porcento a cada pagamento acima de cem

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,11 @@
+import br.com.brrtech.bytebank.io.LeitorCSV;
 import br.com.brrtech.bytebank.modelo.Funcionario;
 import br.com.brrtech.bytebank.modelo.Pagamento;
 import br.com.brrtech.bytebank.registrador.RegistroDePagamento;
 import br.com.brrtech.bytebank.visualizador.OpcoesDePagamento;
 import br.com.brrtech.bytebank.visualizador.OpcoesDePagamento;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -12,10 +14,31 @@ import static br.com.brrtech.bytebank.modelo.Tipo.CREDITO;
 import static br.com.brrtech.bytebank.modelo.Tipo.DEBITO;
 
 public class Principal {
-    public static void main(String[] args) {
-//        System.out.println("Bem vindo ao Bytebank Estudando o INTELIJ \n");
-         new OpcoesDePagamento().mensagemDeBoasVindas();
+    public static void main(String[] args) throws IOException {
+        LeitorCSV leitor = new LeitorCSV();
+        List<Pagamento> pagamentos = leitor.ler(1);
 
+        for (Pagamento pagamento:
+                pagamentos) {
+
+            double valorBruto = pagamento.getValor();
+
+            if (valorBruto > 100) {
+                double valorComAcrescimo = valorBruto * 1.1;
+
+                pagamento.setValor(valorComAcrescimo);
+        }
+
+            System.out.println(pagamento);
+        }
+
+
+
+
+
+
+//         new OpcoesDePagamento().mensagemDeBoasVindas();
+//        System.out.println("Bem vindo ao Bytebank Estudando o INTELIJ \n");
 //        System.out.println("Imprimindo informações dos Funcionários");
 //
 //        Funcionario jose = new Funcionario("José",1, LocalDate.of(1987,03,29));
